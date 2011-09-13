6f7238bf38f85b904fca25eca341606dc0437872
kbl <m.pietraszek@gmail.com>
2011-09-13 10:11:59 +0200

    basic project structure

---
+++ src/pl/nitroit/saintsday/SaintsDayActivity.java
@@ -0,0 +1,13 @@
+package pl.nitroit.saintsday;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SaintsDayActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
