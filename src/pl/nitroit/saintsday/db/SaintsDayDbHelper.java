744513795201d99ece7dffd19fe38e81b5a6e294
kbl <m.pietraszek@gmail.com>
2011-09-13 13:36:34 +0200

    font color moved to res file, basic db helper

---
+++ src/pl/nitroit/saintsday/db/SaintsDayDbHelper.java
@@ -0,0 +1,48 @@
+/**
+ * Sep 13, 2011
+ */
+package pl.nitroit.saintsday.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * @author kbl
+ *
+ */
+public class SaintsDayDbHelper extends SQLiteOpenHelper {
+
+	private static final int VERSION = 1;
+	private static final String DB_NAME = "saints_day.db";
+
+	private static final String CREATE_DB_STMT = "CREATE  TABLE names (" +
+			"_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE," +
+			"day INTEGER NOT NULL," +
+			"month INTEGER NOT NULL," +
+			"name VARCHAR NOT NULL);";
+
+	public SaintsDayDbHelper(Context context) {
+		super(context, DB_NAME, null, VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(CREATE_DB_STMT);
+		seedData(db);
+	}
+
+	private void seedData(SQLiteDatabase db) {
+		try {
+			db.beginTransaction();
+			// TODO_MAPI : insert data into db
+			db.setTransactionSuccessful();
+		} finally {
+			db.endTransaction();
+		}
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) { }
+
+}
