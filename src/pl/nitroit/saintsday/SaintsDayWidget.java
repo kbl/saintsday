b510cd78799b7633262a0463e46fa3f0f9b4f5eb
kbl <m.pietraszek@gmail.com>
2011-09-13 10:37:13 +0200

    basic widget structure

---
+++ src/pl/nitroit/saintsday/SaintsDayWidget.java
@@ -0,0 +1,16 @@
+/**
+ * Sep 13, 2011
+ */
+package pl.nitroit.saintsday;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * @author kbl
+ *
+ */
+public class SaintsDayWidget extends AppWidgetProvider {
+
+
+
+}
