4dfd27239e05d750f50933f885ff956f2b7f67aa
kbl <m.pietraszek@gmail.com>
2011-09-13 13:52:20 +0200

    dao for accessing saints name for particular day

---
+++ src/pl/nitroit/saintsday/db/SaintsDayDao.java
@@ -0,0 +1,56 @@
+/**
+ * Sep 13, 2011
+ */
+package pl.nitroit.saintsday.db;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * @author kbl
+ *
+ */
+public class SaintsDayDao {
+
+	private SaintsDayDbHelper helper;
+	private SQLiteDatabase db;
+
+	public SaintsDayDao(Context context) {
+		helper = new SaintsDayDbHelper(context);
+	}
+
+
+	public SaintsDayDao open() {
+		if(db == null) {
+			db = helper.getReadableDatabase();
+		}
+		return this;
+	}
+
+	public List<String> getSaintsForDate(Integer month, Integer day) {
+		Cursor names = db.query(
+				"names",
+				new String[] { "name" },
+				"WHERE month = ? AND day = ?",
+				new String[] { String.valueOf(month), String.valueOf(day)}, null, null, null);
+
+		return getNamesFromCursor(names);
+	}
+
+	private List<String> getNamesFromCursor(Cursor names) {
+		List<String> returnedNames = new ArrayList<String>(names.getCount());
+
+		int nameColumnIndex = 0;
+		names.moveToFirst();
+		do {
+			returnedNames.add(names.getString(nameColumnIndex));
+		} while(names.moveToNext());
+
+		return returnedNames;
+	}
+
+}
