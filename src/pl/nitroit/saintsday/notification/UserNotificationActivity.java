bc1262eff4f2139d167c46053d230f54b32a3d2a
kbl <m.pietraszek@gmail.com>
2011-09-19 13:31:39 +0200

    canceling notification

--- src/pl/nitroit/saintsday/notification/UserNotificationActivity.java
+++ src/pl/nitroit/saintsday/notification/UserNotificationActivity.java
@@ -6,6 +6,8 @@ package pl.nitroit.saintsday.notification;
 import java.util.List;
 
 import android.app.ListActivity;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 
 /**
@@ -20,6 +22,14 @@ public class UserNotificationActivity extends ListActivity {
 		super.onCreate(savedInstanceState);
 		List<Contact> contactIds = (List<Contact>) getIntent().getSerializableExtra(UserNotifier.CONTACTS_IDS);
 		setListAdapter(new ContactArrayAdapter(this, contactIds));
+
+		cancelNotification();
+	}
+
+	private void cancelNotification() {
+		NotificationManager manager =
+				(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		manager.cancel(UserNotifier.NOTIFICATION_ID);
 	}
 
 }
